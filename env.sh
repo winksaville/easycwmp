297ae20d091b068932fdbc80f1260725db5ad7f5
Wink Saville <wink@saville.com>
2019-10-23 14:01:38 -0700

    Clean up env.sh

--- env.sh
+++ env.sh
@@ -7,5 +7,5 @@ export PATH=$ddir/sbin:$PATH
 export EASYCWMP_INSTALL_DIR=$ddir
 export PKG_CONFIG_PATH=$ddir/lib/pkgconfig:/lib/pkgconfig
 export LD_LIBRARY_PATH=$ddir/lib
-export UCI_CONFIG_DIR="$ddir/etc/config/"
-export UBUS_SOCKET="/var/run/ubus.sock"
+export UCI_CONFIG_DIR=$ddir/etc/config/
+export UBUS_SOCKET=/var/run/ubus.sock
